425cc894e2ffb9c6f3ae453d88362c85bbf62a1f
Ajay <58153961+AJAY-OP@users.noreply.github.com>
2022-07-02 00:20:27 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,6 +6,6 @@ Config.Redeem_Code_Command = "redeemp" -- The command used to redeem a promo cod
 Config.Garage_Version = "new" -- Put this to new if you use the newest garage version
 
 Config['Webhooks'] = {
-	["Redeemed_Code"] = "https://discord.com/api/webhooks/992499168365002833/lICgMq6Hb1eqhHBQyzHzFfbB26qFPRwVCIYOen3PeIDzhxrEwuRjLs8Bimuos3QroCu5",
-	["Created_Code"] = "https://discord.com/api/webhooks/992499029135081573/0p745yrM6ucW4B-vVmXQ9Bk4EuZkEr2SXJ7-zZZmdw3vOLCT0VSH_gYQZ8kCeZzIR-R_"
+	["Redeemed_Code"] = "BLANK",
+	["Created_Code"] = "BLANK"
 }
